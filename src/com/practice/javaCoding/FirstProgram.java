9a41f9ed8cf3f07ee7a9e6582fa851f8e9afbe7b
Abhi@941166 <abhiraj53446@gmail.com>
2025-03-12 13:58:15 +0530

    adding odd or eeven program

--- src/com/practice/javaCoding/FirstProgram.java
+++ src/com/practice/javaCoding/FirstProgram.java
@@ -1,5 +1,23 @@
 package com.practice.javaCoding;
 
+import java.util.Scanner;
+
 public class FirstProgram {
 
+	
+	public class OddEven {
+	    public static void main(String[] args) {
+	        Scanner scanner = new Scanner(System.in);
+	        System.out.print("Enter a number: ");
+	        int num = scanner.nextInt();
+	        
+	        if (num % 2 == 0) {
+	            System.out.println(num + " is Even.");
+	        } else {
+	            System.out.println(num + " is Odd.");
+	        }
+	        
+	        scanner.close();
+	    }
+	}
 }
